7debd80000ba3e8f610b13d2321d202fa5360fc9
IkyyOfficial <gendonmenjeng@gmail.com>
2023-04-19 23:02:38 +0700

    update frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 		const message = { role: "user", content: userValue }
 
 		try {
-			const response = await fetch("https://pgpt-api.onrender.com/api", {
+			const response = await fetch("https://chatky.diki6969.repl.co/api", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json"
